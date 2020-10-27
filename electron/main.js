e410509b9cbe01c6266f0d80c427b81b21f5c77e
lif3ng <lif3ng@icloud.com>
2020-10-27 17:05:59 +0800

    feat: run electron when dev server start

--- electron/main.js
+++ electron/main.js
@@ -1,58 +1,19 @@
 const { app, BrowserWindow, session } = require("electron");
-require("./ipcHander");
 const path = require("path");
-const fs = require("fs");
+require("./ipcHander");
+
 let win;
 
-// session.defaultSession.on("will-download", (event, item, webContents) => {
-//   // event.preventDefault();
-//   // require("request")(item.getURL(), (data) => {
-//   //   require("fs").writeFileSync("/somewhere", data);
-//   // });
-//   item.once("done", (event, state) => {
-//     if (state === "completed") {
-//       const savePath = item.getSavePath();
-//       console.log(savePath);
-//       fs.readFile(savePath, (err, data) => {
-//         const dataStr = data.toString();
-//         console.log({ dataStr });
-//         console.log(JSON.parse(dataStr));
-//       });
-//     }
-//   });
-// });
-module.exports = (url) => {
-  console.log({ app });
-  app.whenReady().then(() => {
-    console.log("whenReady");
-    win = new BrowserWindow({
-      webPreferences: {
-        nodeIntegration: true,
-        webSecurity: false,
-        preload: path.join(__dirname, "preload.js"),
-      },
-    });
-    win.loadURL(url);
-    win.webContents.openDevTools();
-    session.defaultSession.on("will-download", willDownloadHandler);
+const url = process.argv[process.argv.length - 1];
+app.whenReady().then(() => {
+  console.log("whenReady");
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
-};
-
-const willDownloadHandler = (event, item, webContents) => {
-  item.setSavePath(path.join(app.getPath("temp"), `trans${+new Date()}.txt`));
-  console.log("will download");
-  item.on("done", (event, state) => {
-    console.log("done");
-    if (state === "completed") {
-      const savePath = item.getSavePath();
-      console.log(savePath);
-
-      fs.readFile(savePath, (err, data) => {
-        const dataStr = data.toString();
-        console.log({ dataStr });
-
-        win.webContents.send("translate-result", JSON.parse(dataStr));
-      });
-    }
-  });
-};
+  win.loadURL(url);
+  win.webContents.openDevTools();
+});
--- entry.js
+++ entry.js
@@ -1,9 +1,30 @@
-const { fork } = require("child_process");
 const path = require("path");
+const electron = require("electron");
+const spawn = require("cross-spawn");
 
-fork(require.resolve("@vue/cli-service/bin/vue-cli-service.js"), [
-  "serve",
-  path.join(__dirname, "web"),
-]);
+let electronProcess;
+const vueProcess = spawn(
+  require.resolve("@vue/cli-service/bin/vue-cli-service.js"),
+  ["serve", path.join(__dirname, "web")]
+);
 
-require("./electron/main.js")("http://localhost:8080");
+const findDevServerUrl = (data) => {
+  const msg = data.toString();
+  const matchResult = msg.match(/- Local:\W+(.*)\W/);
+  if (matchResult) {
+    console.log(`Run electron with URL: ${matchResult[1]}`);
+    startElectron(matchResult[1]);
+    vueProcess.stdout.removeListener("data", findDevServerUrl);
+  }
+};
+vueProcess.stdout.addListener("data", findDevServerUrl);
+vueProcess.on("error", console.log);
+const startElectron = (url) => {
+  electronProcess = spawn(
+    electron,
+    ["--inspect", require.resolve("./electron/main.js"), url],
+    {
+      stdio: "inherit",
+    }
+  );
+};
